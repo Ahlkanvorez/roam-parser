dd3493679c47b00c429b3a03874b3fa4c3865a62
Robert Mitchell <robert.mitchell36@gmail.com>
2020-12-15 15:24:10 -0600

    Add tests for heterogeneous nesting of symmetric groups

--- test/roam/core_test.clj
+++ test/roam/core_test.clj
@@ -23,38 +23,38 @@
   (merge
    (double-bracket-test-cases open close kind)
    {(str open open "hello" close " roam world" close)
-   {kind [{kind [{:text "hello"}]}
-          {:text " roam world"}]}
+    {kind [{kind [{:text "hello"}]}
+           {:text " roam world"}]}
 
-   (str open "hello " open "roam" close " world" close)
-   {kind [{:text "hello "}
-          {kind [{:text "roam"}]}
-          {:text " world"}]}
+    (str open "hello " open "roam" close " world" close)
+    {kind [{:text "hello "}
+           {kind [{:text "roam"}]}
+           {:text " world"}]}
 
-   (str open "hello roam " open "world" close close)
-   {kind [{:text "hello roam "}
-          {kind [{:text "world"}]}]}
+    (str open "hello roam " open "world" close close)
+    {kind [{:text "hello roam "}
+           {kind [{:text "world"}]}]}
 
-   (str open open open "three" close " two" close " one" close)
-   {kind [{kind [{kind [{:text "three"}]}
-                 {:text " two"}]}
-          {:text " one"}]}
+    (str open open open "three" close " two" close " one" close)
+    {kind [{kind [{kind [{:text "three"}]}
+                  {:text " two"}]}
+           {:text " one"}]}
 
-   (str open open "two " open "three" close close " one" close)
-   {kind [{kind [{:text "two "}
-                 {kind [{:text "three"}]}]}
-          {:text " one"}]}
+    (str open open "two " open "three" close close " one" close)
+    {kind [{kind [{:text "two "}
+                  {kind [{:text "three"}]}]}
+           {:text " one"}]}
 
-   (str open "one " open "two " open "three" close close close)
-   {kind [{:text "one "}
-          {kind [{:text "two "}
-                 {kind [{:text "three"}]}]}]}
+    (str open "one " open "two " open "three" close close close)
+    {kind [{:text "one "}
+           {kind [{:text "two "}
+                  {kind [{:text "three"}]}]}]}
 
-   (str "Nesting! " open "one " open "two " open "three" close close close)
-   {:tree [{:text "Nesting! "}
-           {kind [{:text "one "}
-                  {kind [{:text "two "}
-                         {kind [{:text "three"}]}]}]}]}}))
+    (str "Nesting! " open "one " open "two " open "three" close close close)
+    {:tree [{:text "Nesting! "}
+            {kind [{:text "one "}
+                   {kind [{:text "two "}
+                          {kind [{:text "three"}]}]}]}]}}))
 
 (deftest syntax-link
   (doseq [[text tree] (nested-double-bracket-test-cases "[[" "]]" :link)]
@@ -83,6 +83,56 @@
 (deftest syntax-italic
   (doseq [[text tree] (double-bracket-test-cases "__" "__" :italic)]
     (is (= tree (core/parse text)))))
+
+(defn syntax-nesting-test-cases [group1 group2 group3]
+  (let [[open1 close1 kind1] group1
+        [open2 close2 kind2] group2
+        [open3 close3 kind3] group3]
+    {(str open1 open2 "hello" close2 " roam world" close1)
+     {kind1 [{kind2 [{:text "hello"}]}
+             {:text " roam world"}]}
+
+     (str open1 "hello " open2 "roam" close2 " world" close1)
+     {kind1 [{:text "hello "}
+             {kind2 [{:text "roam"}]}
+             {:text " world"}]}
+
+     (str open1 "hello roam " open2 "world" close2 close1)
+     {kind1 [{:text "hello roam "}
+             {kind2 [{:text "world"}]}]}
+
+     (str open1 open2 open3 "three" close3 " two" close2 " one" close1)
+     {kind1 [{kind2 [{kind3 [{:text "three"}]}
+                     {:text " two"}]}
+             {:text " one"}]}
+
+     (str open1 open2 "two " open3 "three" close3 close2 " one" close1)
+     {kind1 [{kind2 [{:text "two "}
+                     {kind3 [{:text "three"}]}]}
+             {:text " one"}]}
+
+     (str open1 "one " open2 "two " open3 "three" close3 close2 close1)
+     {kind1 [{:text "one "}
+             {kind2 [{:text "two "}
+                     {kind3 [{:text "three"}]}]}]}
+
+     (str "Nesting! " open1 "one " open2 "two " open3 "three" close3 close2 close1)
+     {:tree [{:text "Nesting! "}
+             {kind1 [{:text "one "}
+                     {kind2 [{:text "two "}
+                             {kind3 [{:text "three"}]}]}]}]}}))
+
+(deftest syntax-nesting
+  (let [latex ["$$" "$$" :latex]
+        highlight ["^^" "^^" :highlight]
+        bold ["**" "**" :bold]
+        italic ["__" "__" :italic]
+        groups [latex highlight bold italic]]
+    (doseq [a groups]
+      (doseq [b (remove #{a} groups)]
+        (doseq [c (remove #{a b} groups)]
+          (doseq [[text tree] (syntax-nesting-test-cases a b c)]
+            (is (= tree (core/parse text)))))))))
 
 (deftest syntax-alias
   (is (= :todo :todo)))
