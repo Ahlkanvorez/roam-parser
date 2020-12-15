8e9be6808a6f4fc7a2c735a31628e47059388313
Robert Mitchell <robert.mitchell36@gmail.com>
2020-12-15 13:47:04 -0600

    Add quick & dirty parser & serializer for nested [[links]]

--- src/roam/core.clj
+++ src/roam/core.clj
@@ -1,2 +1,98 @@
-(ns roam.core)
+(ns roam.core
+  (:require [clojure.string :as string]))
 
+(def group-complement
+  {"]]" "[["})
+
+(defn group-start? [s]
+  (some #(string/starts-with? s %)
+        (vals group-complement)))
+
+(defn group-end? [s]
+  (some #(string/starts-with? s %)
+        (keys group-complement)))
+
+(def open-for (partial get group-complement))
+(def type-for {"]]" :link})
+
+(defn aggregate-groups [accum]
+  (->> (reduce (fn [accum d]
+                 (println :reduce accum d)
+                 (let [prior (or (last accum) "")]
+                   (if (and (string? prior)
+                            (string? d))
+                     (conj (vec (butlast accum))
+                           (str prior d))
+                     (conj accum d))))
+               []
+               accum)
+       (mapv (fn [n]
+               (if (string? n)
+                 {:text n}
+                 n)))))
+
+(defn parse-group [stack end]
+  (let [target (open-for end)]
+    (loop [stack (conj stack end)
+           accum ()]
+      (println :parse-group stack accum)
+      (if-let [c (first stack)]
+        (cond
+          (= c target)
+          {:node {(type-for end) (aggregate-groups (butlast accum))}
+           :rest (rest stack)}
+          :default (recur (rest stack) (conj accum c)))
+        (loop [accum accum
+               nodes []]
+          (println :accum accum)
+          {:node {:text (apply str accum)}
+           :rest []})))))
+
+(defn tokens->tree [tokens]
+  (loop [accum ()
+         tokens (reverse tokens)]
+    (println :tokens->tree accum tokens)
+    (if-let [v (first tokens)]
+      (cond
+        (group-end? v)
+        (let [subparse (parse-group accum v)]
+          (println :subparse subparse)
+          (if-not (empty? (:rest subparse))
+            (recur (conj (:rest subparse) (:node subparse))
+                   (rest tokens))
+            (recur [(:node subparse)]
+                   (rest tokens))))
+        :default (recur (conj accum v)
+                        (rest tokens)))
+      (if (= (count accum) 1)
+        (first accum)
+        (let [aggregate (aggregate-groups (reverse accum))]
+          (if (= (count aggregate) 1)
+            (first aggregate)
+            {:tree aggregate}))))))
+
+(defn str->tokens [s]
+  (loop [s s
+         stack ()]
+    (if (empty? s)
+      stack
+      (if (or (group-start? s) (group-end? s))
+        (recur (subs s 2)
+               (conj stack (subs s 0 2)))
+        (recur (subs s 1)
+               (conj stack (subs s 0 1)))))))
+
+(defn str->tree [s]
+  (-> s str->tokens tokens->tree))
+
+(defmulti tree->str (comp first keys))
+
+(defmethod tree->str :text [tree] (:text tree))
+(defmethod tree->str :link [tree]
+  (str "[["
+       (apply str (map tree->str (:link tree)))
+       "]]"))
+(defmethod tree->str :tree [tree]
+  (apply str (map tree->str (:tree tree))))
+
+(def parse str->tree)
--- test/roam/core_test.clj
+++ test/roam/core_test.clj
@@ -2,9 +2,98 @@
   (:require [clojure.test :refer :all]
             [roam.core :as core]))
 
-(deftest hello
-  (is (= (name :a) "a"))
-  (is (= (name :b) "b")))
+(deftest raw-text
+  (is (= {:text "abc"}
+         (core/parse "abc")))
+  (is (= {:text "123"}
+         (core/parse "123")))
+  (is (= {:text "abc-123"}
+         (core/parse "abc-123"))))
 
-(deftest cheese
-  (is (> (count "cheddar") (count "brie"))))
+(deftest syntax-link
+  (is (= {:link [{:text "hello"}]}
+         (core/parse "[[hello]]")))
+  (is (= {:tree [{:text "abc "}
+                 {:link [{:text "hello"}]}
+                 {:text " 123"}]}
+         (core/parse "abc [[hello]] 123"))))
+
+(deftest syntax-link-recursive
+  (is (= {:link [{:text "hello roam "}
+                 {:link [{:text "world"}]}]}
+         (core/parse "[[hello roam [[world]]]]")))
+  (is (= {:link [{:text "hello "}
+                 {:link [{:text "roam"}]}
+                 {:text " world"}]}
+         (core/parse "[[hello [[roam]] world]]")))
+  (is (= {:link [{:link [{:text "hello"}]}
+                 {:text " roam world"}]}
+         (core/parse "[[[[hello]] roam world]]")))
+  (is (= {:link [{:link [{:link [{:text "three"}]}
+                         {:text " two"}]}
+                 {:text " one"}]}
+         (core/parse "[[[[[[three]] two]] one]]")))
+  (is (= {:link [{:link [{:text "two "}
+                         {:link [{:text "three"}]}]}
+                 {:text " one"}]}
+         (core/parse "[[[[two [[three]]]] one]]")))
+  (is (= {:link [{:text "one "}
+                 {:link [{:text "two "}
+                         {:link [{:text "three"}]}]}]}
+         (core/parse "[[one [[two [[three]]]]]]")))
+  (is (= {:tree [{:text "Nesting! "}
+                 {:link [{:text "one "}
+                         {:link [{:text "two "}
+                                 {:link [{:text "three"}]}]}]}]}
+         (core/parse "Nesting! [[one [[two [[three]]]]]]"))))
+
+(deftest syntax-ref
+  (is (= :todo :todo)))
+
+(deftest syntax-roam-render
+  (is (= :todo :todo)))
+
+(deftest syntax-latex
+  (is (= :todo :todo)))
+
+(deftest syntax-alias
+  (is (= :todo :todo)))
+
+(deftest syntax-highlight
+  (is (= :todo :todo)))
+
+(deftest syntax-bold
+  (is (= :todo :todo)))
+
+(deftest syntax-italic
+  (is (= :todo :todo)))
+
+(deftest syntax-recursive
+  (is (= :todo :todo)))
+
+(deftest tree-abstraction
+  (is (= :todo :todo)))
+
+(deftest parse-speed
+  ;; Must parse 1000 strings in under 500ms
+  (is (= :todo :todo)))
+
+(deftest tree->str
+  (is (= "abc"
+         (core/tree->str {:text "abc"})))
+  (is (= "[hello]"
+         (core/tree->str {:text "[hello]"})))
+  (is (= "[[hello links]]"
+         (core/tree->str {:link [{:text "hello links"}]})))
+  (is (= "[[hello [[nested [[links]]]]]]"
+         (core/tree->str {:link [{:text "hello "}
+                                 {:link [{:text "nested "}
+                                         {:link [{:text "links"}]}]}]})))
+  (is (= "this is [[probably [[[[enough]] linking]] for]] now."
+         (core/tree->str
+          {:tree [{:text "this is "}
+                  {:link [{:text "probably "}
+                          {:link [{:link [{:text "enough"}]}
+                                  {:text " linking"}]}
+                          {:text " for"}]}
+                  {:text " now."}]}))))
